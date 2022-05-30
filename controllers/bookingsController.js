4f07e82482b2041c5d1ead25a71f5c85b9d150b7
Vadim <43332729+BladeRunner95@users.noreply.github.com>
2022-05-30 18:59:46 +0300

    booking rest api

--- controllers/bookingsController.js
+++ controllers/bookingsController.js
@@ -1,25 +1,46 @@
 const asyncHandler = require('express-async-handler')
+
+const Booking = require('../models/bookingModel')
+
 //GET /api/bookings
 const getBookings = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Get Goals'});
+    const bookings = await Booking.find()
+    res.status(200).json(bookings);
 })
 //SET /api/bookings
 const setBookings = asyncHandler(async (req, res) => {
-    if (!req.body.pipi) {
+    if (!req.body.text) {
         res.status(400)
             throw new Error('Please add a pipi field')
     } else {
-        res.status(200).json({message: 'Set goal'})
+        const booking = await Booking.create({
+            text: req.body.text
+        })
+        res.status(200).json(booking)
     }
 })
 
 //PUT /api/bookings/id
 const changeBookings = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Update Goal ${req.params.id}`})
+    const booking = await Booking.findById(req.params.id)
+    if (!booking) {
+        res.status(400);
+        throw new Error('Booking not found')
+    }
+    const updateBooking = await Booking.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+    })
+        res.status(200).json(updateBooking)
 })
 //DELETE /api/bookings/id
 const deleteBookings = asyncHandler(async (req, res) => {
-    res.status(200).json({message: `Delete Goal ${req.params.id}`})
+    const booking = Booking.findById(req.params.id)
+    if (!booking) {
+        res.status(400)
+        throw new Error('Booking not found')
+    }
+    await booking.remove()
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
